610438900e5e9ace3cdeaad57fbf938cbb7524c6
r2h2 <rainer@hoerbe.at>
2016-07-20 14:30:31 +0200

    remove --rm run option

--- exec.sh
+++ exec.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
-# rhoerbe/docker-template@github 2016-07-11
+# rhoerbe/docker-template@github 2016-07-20
 
-EXECCMD=/bin/bash
-while getopts ":hin:pr" opt; do
+while getopts ":hin:prR" opt; do
   case $opt in
     i)
-      runopt='-it'
+      runopt='-it --rm'
       ;;
     n)
-      config_nr=$OPTARG
       re='^[0-9][0-9]$'
       if ! [[ $OPTARG =~ $re ]] ; then
-         echo "error: -n argument is not a number in the range frmom 02 .. 99" >&2; exit 1
+         echo "error: -n argument ($OPTARG) is not a number in the range frmom 02 .. 99" >&2; exit 1
       fi
+      config_nr=$OPTARG
       ;;
     p)
       print="True"
@@ -20,41 +19,56 @@ while getopts ":hin:pr" opt; do
     r)
       useropt='-u 0'
       ;;
+    R)
+      remove='True'
+      ;;
     :)
       echo "Option -$OPTARG requires an argument"
       exit 1
       ;;
     *)
-      echo "usage: $0 [-h] [-i] [-n] [-p] [-r] [cmd]
+      echo "usage: $0 [-h] [-i] [-p] [-r] [cmd]
    -h  print this help text
-   -i  interactive (results in options -i -t for docker exec)
+   -i  start in interactive mode and remove container afterwards
    -n  configuration number ('<NN>' in conf<NN>.sh)
-   -p  print docker exec command on stdout
-   -r  execute as root user
-   cmd shell command to be executed (default is $EXECCMD)
-   unknow option $opt
-   "
+   -p  print docker run command on stdout
+   -r  start command as root user (default is $CONTAINERUSER)
+   -R  remove dangling container before start
+   cmd shell command to be executed (default is $STARTCMD)
+   unknow option $opt"
       exit 0
       ;;
   esac
 done
-
 shift $((OPTIND-1))
 
 SCRIPTDIR=$(cd $(dirname $BASH_SOURCE[0]) && pwd)
 source $SCRIPTDIR/conf${config_nr}.sh
 
+if [ -z "$runopt" ]; then
+    runopt='-d --restart=unless-stopped'
+fi
+if [ -z "$useropt" ]; then
+    useropt="-u $CONTAINERUSER"
+fi
 if [ -z "$1" ]; then
-    cmd=$EXECCMD
+    cmd=$STARTCMD
 else
     cmd=$@
 fi
-docker_exec="docker exec $runopt $useropt $CONTAINERNAME $cmd"
+docker_run="docker run $runopt $useropt --hostname=$CONTAINERNAME --name=$CONTAINERNAME
+    $ENVSETTINGS $NETWORKSETTINGS $VOLMAPPING $IMAGENAME $cmd"
 
 if [ $(id -u) -ne 0 ]; then
     sudo="sudo"
 fi
+$sudo docker rm -f $CONTAINERNAME 2>/dev/null || true
 if [ "$print" = "True" ]; then
-    echo $docker_exec
+    echo $docker_run
 fi
-${sudo} $docker_exec
+# remove dangling container
+if [ -e $remove ]; then
+    docker ps -a | grep $CONTAINERNAME > /dev/null && docker rm $CONTAINERNAME
+fi
+
+$sudo $docker_run
