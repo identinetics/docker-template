865545514c2eec84200c83ad7288d273264c2aea
r2h2 <rainer@hoerbe.at>
2017-01-18 12:59:44 +0100

    +sshd

--- templates/install/scripts/start_sshd.sh
+++ templates/install/scripts/start_sshd.sh
@@ -1,28 +1,18 @@
 #!/usr/bin/env bash
 
-# create config files if missing
-# sshd key files need to be in external storage to reuse exising key material if container is destroyed and re-created
+# create keys if missing
 
-if [ ! -e /opt/ssh/sshd_config ]; then
-    cp /etc/ssh/sshd_config /opt/ssh/sshd_config
-    echo 'GSSAPIAuthentication no' >> /opt/ssh/sshd_config
-    echo 'useDNS no' >> /opt/ssh/sshd_config
-fi
-[ -e /opt/ssh/ssh_host_rsa_key ] || ssh-keygen -q -N '' -t rsa -f /opt/ssh/ssh_host_rsa_key
-[ -e /opt/ssh/ssh_host_ecdsa_key ] || ssh-keygen -q -N '' -t ecdsa -f /opt/ssh/ssh_host_ecdsa_key
-[ -e /opt/ssh/ssh_host_ed25519_key ] || ssh-keygen -q -N '' -t ed25519 -f /opt/ssh/ssh_host_ed25519_key
+[ -e /etc/ssh/ssh_host_rsa_key ] || ssh-keygen -q -N '' -t rsa -f /etc/ssh/ssh_host_rsa_key
+[ -e /etc/ssh/ssh_host_ecdsa_key ] || ssh-keygen -q -N '' -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key
+[ -e /etc/ssh/ssh_host_ed25519_key ] || ssh-keygen -q -N '' -t ed25519 -f /etc/ssh/ssh_host_ed25519_key
 
 
 
 echo 'starting sshd in foreground'
-/usr/sbin/sshd -f /opt/ssh/sshd_config \
-   -p 2022 \
-   -h /opt/ssh/ssh_host_rsa_key \
-   -h /opt/ssh/ssh_host_ecdsa_key \
-   -h /opt/ssh/ssh_host_ed25519_key
+/usr/sbin/sshd -p 2022
 
 echo 'ready to login, like:'
-echo 'ssh -o "StrictHostKeyChecking no" -i ~/.ssh/id_ed25519_loopback -p 2022 user13@localhost'
+echo 'ssh -o "StrictHostKeyChecking no" -i ~/.ssh/id_ed25519_loopback -p 2022 <someuser>@thishost'
 
-echo 'exiting this shell will terminate the container'
+echo 'exiting this shell may terminate the container'
 bash
