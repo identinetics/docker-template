c3758f41d9a56de650223b263fa5aa9afc53676e
r2h2 <rainer@hoerbe.at>
2017-02-14 09:21:10 +0100

    ignore missing image

--- get_capabilities.py
+++ get_capabilities.py
@@ -6,5 +6,5 @@ import sys
 metadata = json.loads(sys.stdin.read())
 try:
     print(metadata[0]['ContainerConfig']['Labels']['capabilites'])
-except KeyError IndexError:
+except (KeyError, IndexError):
     pass
