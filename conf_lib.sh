b5b3d50cdf56b537d0621518554d6509e0b60ff6
r2h2 <rainer@hoerbe.at>
2017-04-29 08:23:06 +0200

    no fs-ops on docker vol for jenkins

--- conf_lib.sh
+++ conf_lib.sh
@@ -166,7 +166,7 @@ map_docker_volume() {
     if [[ "$TRAVIS" != "true" ]] && [[ ! -z ${JENKINS_HOME+x} ]]; then
         fs_access="--symlink --prefix $PREFIX $symlink --groupwrite"
     fi
-    $sudo $CONFLIBDIR/docker_vol_mount.py fs_access $chcon_opt --volume $VOL_NAME
+    $sudo $CONFLIBDIR/docker_vol_mount.py $fs_access $chcon_opt --volume $VOL_NAME
 }
 
 
